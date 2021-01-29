267796f5089e6935258521b669f4d041ccdb1fd2
Mohammad Tariq <m.faixal16212@gmail.com>
2021-01-29 21:26:23 +0500

    Navigation DOM & Events

---
+++ objects,arrays,functions.js
@@ -0,0 +1,42 @@
+//Objects
+
+
+let student = {
+      
+    name: "M Tariq",
+    roll: 4241,
+    department: "Information Technology",
+    semester:3,
+
+
+
+
+
+}
+
+console.log(student);
+
+
+//Arrays
+
+
+let friends = ["Asad","Adnan", "Ahmad", "Hanzla","M Tariq"];
+
+
+console.log(friends[4]);
+
+
+//Functions
+
+
+function info(id,greeting){
+    console.log(id +" "+ "Is a good boy");;
+    console.log(id+ ","+greeting);
+}
+
+
+let id = "M Tariq";
+greeting = "JS said is a good boy ";
+
+
+info(id,greeting);
